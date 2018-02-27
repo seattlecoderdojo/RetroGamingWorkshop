d1d39bd7cd3b7326a0627eef511616f5f9455f62
Greg Bulmash <irv@yiddish.ninja>
2018-02-26 19:46:08 -0800

    setting base game.js

---
+++ week2/scripts/game.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+  var game = new Phaser.Game(900, 700, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+  function preload(){
+
+  }
+
+  function create(){
+
+  }
+
+  function update(){
+
+  }
+
+
+
+
+}
